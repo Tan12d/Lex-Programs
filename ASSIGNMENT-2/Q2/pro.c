599cab3ccd38a62ffbaedf84be2fe0e9e3dd98f0
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-16 04:22:25 +0530

    Add files via upload

---
+++ ASSIGNMENT-2/Q2/pro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,p;	
+	scanf("%d",&n);
+scanf("%d",&p);
+	printf("yu");
+printf("u");
+printf("1yu");
+}
---
+++ ASSIGNMENT-2/Q2/pro1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,p;	
+	readf("%d",&n);
+readf("%d",&p);
+	writef("yu");
+writef("u");
+writef("1yu");
+}
